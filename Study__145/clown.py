3ba92b43cac02ec25ead48d4767f5b59bdc5c477
이세현 <86671456+qsdcfd@users.noreply.github.com>
2022-01-28 00:20:22 +0900

    Add files via upload

---
+++ Study__145/clown.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+bigcount = None
+bigword = None
+for word,count in counts.items():
+ if bigcount is None or count > bigcount:
+     bigword = word
+     bigcount = count
+print(bigword, bigcount)
+
---
+++ Study__145/words.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+name = input('Enter file:')
+handle = open(name)
+
+counts = dict()
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word,0) + 1
+
