b128063f0c171543b56322640280d294d9ba1526
이세현 <86671456+qsdcfd@users.noreply.github.com>
2022-07-19 19:43:56 +0900

    Add files via upload

---
+++ Study__317/basic_nodes.py
@@ -0,0 +1,28 @@
+class plus_node:
+    def __init__(self):
+    def forward(self,x,y):
+    def backward(self,dz):
+
+
+class minus_node:
+    def __init__(self):
+    def forward(self, x,y):
+    def backward(self, dz):
+
+
+class mul_node:
+    def __init__(self):
+    def forward(self, x,y):
+    def backward(self, dz):
+
+
+class square_node:
+    def __init__(self):
+    def forward(self,x):
+    def backward(self,dz):
+
+
+class mean_node:
+    def __init__(self):
+    def forward(self, x):
+    def backward(self, dz):
